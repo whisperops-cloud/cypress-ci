265e3d6dc46e1117144c99703c27d04dcbc20847
whiserops-cloud <jerome.quandalle@whisperops.cloud>
2026-02-16 21:28:30 +0100

    cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,13 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  allowCypressEnv: false,
-
+  video: true, // Active l'enregistrement vidéo
+  videoCompression: 32, // Optionnel : compresse pour réduire la taille (0 à 51)
+  videosFolder: 'cypress/videos', // Tu peux même changer le dossier ici
+  screenshotOnRunFailure: true,
+  
   e2e: {
+
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -1,5 +1,8 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    cy.visit('http://localhost:3000/')
+    cy.get('[name="username"]').type('Usertest')
+    cy.get('[name="password"]').type('1234')
+    cy.get('button').click()
   })
 })
